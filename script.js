3d8da31440dd40325f912dc00793a3ddbc4daeda
ltmervin123 <ltmervin123@gmail.com>
2024-07-16 08:31:36 +0800

    Finish and ready to submit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var hamburgerIcon = document.getElementById("menu-icon");
+  var xIcon = document.getElementById("close-icon");
+  var navContainer = document.getElementById("side-bar");
+
+  // This function handle the toggle of the Hamburger icon
+  function toggleMenu() {
+    navContainer.classList.toggle("active");
+    hamburgerIcon.style.display =
+      hamburgerIcon.style.display === "none" ? "unset" : "none";
+    xIcon.style.display = xIcon.style.display === "none" ? "unset" : "none";
+  }
+
+  // This function will reset the icon to its original state when the Width size is greater than 475px
+  function resetIcons() {
+    if (window.innerWidth > 768) {
+      navContainer.classList.remove("active");
+      hamburgerIcon.style.display = "none";
+      xIcon.style.display = "none";
+    } else if (!navContainer.classList.contains("active")) {
+      hamburgerIcon.style.display = "unset";
+      xIcon.style.display = "none";
+    }
+  }
+
+  //Add all functionality to the website
+  if (hamburgerIcon && xIcon && navContainer) {
+    hamburgerIcon.addEventListener("click", toggleMenu);
+    xIcon.addEventListener("click", toggleMenu);
+    window.addEventListener("resize", resetIcons);
+    resetIcons(); // Initial check when the page loads
+  } else {
+    console.error(
+      "Element(s) not found: hamburgerIcon, xIcon, or navContainer"
+    );
+  }
+});
